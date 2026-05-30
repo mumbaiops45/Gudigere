16d91afe03f8da98c55b41dd6daa13af5b877c56
mumbaiops45 <mumbaiops45@gmail.com>
2026-05-30 14:52:06 +0530

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   // baseURL:
   //   "http://localhost:5000/api",
-    baseURL:"https://goodiegear-2.onrender.com",
+    baseURL:"https://goodiegear-2.onrender.com/api",
 
   withCredentials: true,
 });
